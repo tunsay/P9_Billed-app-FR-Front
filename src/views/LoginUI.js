8aa5dbeeb01091f23b832de9c7ef2416bf593da8
tunsay <tuna1997@hotmail.fr>
2022-12-31 21:20:24 +0100

    add novalidate for testing

--- src/views/LoginUI.js
+++ src/views/LoginUI.js
@@ -16,7 +16,7 @@ export default () => {
           <div class="col-sm-6">
             <div class="card">
               <div class="card-body">
-                <form class="form-signin" data-testid="form-employee">
+                <form class="form-signin" data-testid="form-employee" novalidate>
                   <h2 class="h3 mb-3 font-weight-normal">Employé</h1>
                   <label for="inputEmail">Votre email</label>
                   <input type="email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
